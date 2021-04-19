826ea569be0393b8d8917b9e7c38199f8be42d19
Joshua Grant <jc.grant22@gmail.com>
2021-04-19 17:48:24 -0400

    clean  up naming

--- src/utils/tokenMapUtils.ts
+++ src/utils/tokenMapUtils.ts
@@ -11,7 +11,7 @@ export function emptyCoordinate(coord: Coordinate, tokenMap: TokenMap): boolean 
     return Object.values(tokenMap).find(entry => entry.coord && entry.coord.x === coord.x && entry.coord.y === coord.y && entry.coord.grid.id === coord.grid.id) === undefined;
 }
 
-export function cFlip(color: string): number {
+export function colorFlip(color: string): number {
     return color === white ? 1 : -1;
 }
 
