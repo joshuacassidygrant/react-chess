c38f219d90e7376b272ec64709f50d11ad89c94e
Joshua Grant <jc.grant22@gmail.com>
2021-05-03 12:32:54 -0400

    Debug text labels on board

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -34,18 +34,20 @@ export const Grid: FC<GridProps> = React.memo(({highlightCells, children}): Reac
         <g transform={`translate(${grid.xOffset},${grid.yOffset})`}>
             {
                 cellsMap.flat().map((cell: CellProps) => 
-                    <rect key={`c${cell.x}${cell.y}`} x={cell.x * grid.xCellWidth} y={cell.y * grid.yCellHeight} width={grid.xCellWidth}  height={grid.yCellHeight} stroke="white" strokeWidth={0} fill={inLegalCells(highlightCells, cell.x, cell.y) ? "red": cell.color}>
-                            <animate attributeName="opacity"
-                               to="0.5" begin="mouseover" dur="0.15s" fill="freeze"/>
-                            <animate attributeName="stroke-width"
-                               to="2" begin="mouseover" dur="0.15s" fill="freeze"/>
+                    <>
+                        <rect key={`c${cell.x}${cell.y}`} x={cell.x * grid.xCellWidth} y={cell.y * grid.yCellHeight} width={grid.xCellWidth}  height={grid.yCellHeight} stroke="white" strokeWidth={0} fill={inLegalCells(highlightCells, cell.x, cell.y) ? "red": cell.color}>
+                                <animate attributeName="opacity"
+                                to="0.5" begin="mouseover" dur="0.15s" fill="freeze"/>
+                                <animate attributeName="stroke-width"
+                                to="2" begin="mouseover" dur="0.15s" fill="freeze"/>
 
-                            <animate attributeName="opacity"
-                                to="1" begin="mouseout" dur="0.15s" fill="freeze"/>
-                            <animate attributeName="stroke-width"
-                               to="0" begin="mouseout" dur="0.15s" fill="freeze"/>
-                               
-                    </rect>
+                                <animate attributeName="opacity"
+                                    to="1" begin="mouseout" dur="0.15s" fill="freeze"/>
+                                <animate attributeName="stroke-width"
+                                to="0" begin="mouseout" dur="0.15s" fill="freeze"/>
+                        </rect>
+                        <text x={cell.x * grid.xCellWidth + grid.xCellWidth/2} y={cell.y * grid.yCellHeight + grid.yCellHeight/2}>{cell.x},{cell.y}</text>
+                    </>
                 )
             }
             {children}
